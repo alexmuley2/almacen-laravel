cd7587561edaa89bd239f4168ed1ed2e213459b8
alexmuley2 <98661311+alexmuley2@users.noreply.github.com>
2024-01-15 14:04:50 +0100

    Add files via upload

---
+++ web.php
@@ -0,0 +1,19 @@
+<?php
+// routes/web.php
+
+use App\Http\Controllers\ProductoController;
+Route::get('/', [ProductoController::class, 'gestionAlmacen'])->name('gestion-almacen');
+
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos.create');
+Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
+Route::get('/productos/{producto}/edit', [ProductoController::class, 'edit'])->name('productos.edit');
+Route::put('/productos/{producto}', [ProductoController::class, 'update'])->name('productos.update');
+Route::delete('/productos/{producto}', [ProductoController::class, 'destroy'])->name('productos.destroy');
+
+
+
+
+
+
+
